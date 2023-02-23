6505b03ae156a209f7356a5d1307edcd6430af3d
Brendan <brencodeert@outlook.com>
2023-02-23 17:45:33 +0100

    Added custom exception

--- src/delft_fiat/error.py
+++ src/delft_fiat/error.py
@@ -1,2 +1,8 @@
-class FIATError(Exception):
-    pass
+class DriverNotFoundError(Exception):
+    def __init__(self, gog, path):
+        self.base = f"{gog} data"
+        self.msg = f"Extension of file: {path.name} is not recoqnized"
+        super(DriverNotFoundError, self).__init__(self.base)
+
+    def __str__(self):
+        return f"{self.base} -> {self.msg}"
