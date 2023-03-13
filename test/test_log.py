8a4c5121139b60e61a4d43fab04454623df4a506
Brendan <brencodeert@outlook.com>
2023-03-13 11:10:21 +0100

    Throw away update

--- test/test_log.py
+++ test/test_log.py
@@ -1,10 +1,14 @@
 import pytest
-from delft_fiat.log import StreamLogger
+from delft_fiat.log import StreamLogger, Log
 
 
 def test_stream():
     assert 1 == 1
 
 
-def test_log():
-    assert 1 == 1
+def test_log(tmpdir):
+    log = Log(str(tmpdir), "Delft_fiat")
+    log.Info("Start of test logging")
+    log.Warning("Warning about stuff")
+    log.Error("Something has really gone wrong...")
+    log.Dead("Bye")
