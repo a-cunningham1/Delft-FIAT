c6dc954b312ea8d754db6810eefe2d9e7f95e0ab
Brendan <brencodeert@outlook.com>
2023-03-29 09:23:55 +0200

    Updated 'test_log.py'

--- test/test_log.py
+++ test/test_log.py
@@ -1,15 +1,40 @@
+from delft_fiat.log import StreamLogger, Log, spawn_child_logger
+
+import io
 import pytest
-from delft_fiat.log import StreamLogger, Log
+from pathlib import Path
 
 
 def test_stream():
-    assert 1 == 1
+    stream = io.StringIO()
+    sl = StreamLogger(stream=stream)
 
+    sl.emit("Hello!\n")
+    sl.emit("Good day!\n")
+
+    stream.seek(0)
+    assert stream.readline().strip() == "Hello!"
 
 def test_log(tmpdir):
-    log = Log("fiat")
-    log.add_loggers(str(tmpdir))
+    log = Log("fiat", log_level=2)
+    log.add_loggers(
+        str(tmpdir),
+        cmd_stream=True,
+    )
+    child_log = spawn_child_logger("fiat.child")
+
+    assert id(log) != id(child_log)
+
+    log.debug("This message should not be output")
     log.info("Start of test logging")
     log.warning("Warning about stuff")
     log.error("Something has really gone wrong...")
+    child_log.error("I also quit!")
     log.dead("Bye")
+
+    del child_log
+    del log
+
+    fh = open(Path(str(tmpdir), "fiat.log"), mode="r")
+
+    assert sum(1 for _ in fh) == 5
