997653421f1d795d0d6e628d4994a1fcb3e40123
Brendan <brencodeert@outlook.com>
2023-06-20 17:16:07 +0200

    raw draft for the grid model

--- src/delft_fiat/models/grid.py
+++ src/delft_fiat/models/grid.py
@@ -1,5 +1,40 @@
+from delft_fiat.io import BufferTextHandler, open_grid
+from delft_fiat.log import spawn_logger
 from delft_fiat.models.base import BaseModel
+from delft_fiat.models.calc import get_inundation_depth, get_damage_factor
+from delft_fiat.util import _pat, replace_empty
+
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Process, get_context
+
+logger = spawn_logger("fiat.model.grid")
+
+
+def worker(haz, vul, exp):
+    pass
 
 
 class GridModel(BaseModel):
-    pass
+    def __init__(
+        self,
+        cfg: "ConfigReader",
+    ):
+        """_summary_"""
+        pass
+
+    def __del__(self):
+        BaseModel.__del__(self)
+
+    def _read_exposure_grid(self):
+        """_summary_"""
+
+        path = self._cfg.get("exposure.raster.file")
+        data = open_grid(path)
+        ## checks
+
+        self._exposure_grid = data
+
+    def run(self):
+        """_summary_"""
+
+        worker()
