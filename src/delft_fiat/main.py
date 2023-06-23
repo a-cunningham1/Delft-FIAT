173ba1c161185a8974c799c139e700e90e9d2a8a
Brendan <brencodeert@outlook.com>
2023-06-23 17:03:56 +0200

    Take ConfigReader by default

--- src/delft_fiat/main.py
+++ src/delft_fiat/main.py
@@ -6,20 +6,34 @@ from pathlib import Path
 
 
 class FIAT:
-    def __init__(self, file: str):
+    def __init__(self, cfg: ConfigReader):
         """_summary_
 
         Parameters
         ----------
-        file : str
+        cfg : ConfigReader
             _description_
         """
+
+        self.cfg = cfg
+
+    @classmethod
+    def from_path(
+        cls,
+        file: str,
+    ):
+        """_summary_"""
+
         file = Path(file)
         if not Path(file).is_absolute():
             file = Path(Path.cwd(), file)
-        self.cfg = ConfigReader(file)
+        cfg = ConfigReader(file)
+
+        return cls(cfg)
 
     def run(self):
+        """_summary_"""
+
         model = GeomModel(self.cfg)
         model.run()
 
