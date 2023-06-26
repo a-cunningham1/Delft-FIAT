dcc0c4c3ffe3d5aab640c6aa1a8b03de2a393a86
Brendan <brencodeert@outlook.com>
2023-06-26 17:00:56 +0200

    Determine srs method for model

--- src/delft_fiat/models/base.py
+++ src/delft_fiat/models/base.py
@@ -1,4 +1,5 @@
 from delft_fiat.check import (
+    check_global_crs,
     check_hazard_subsets,
     check_srs,
 )
@@ -22,12 +23,8 @@ class BaseModel(metaclass=ABCMeta):
 
         self._cfg = cfg
 
-        _srs = self._cfg.get("global.crs")
-        self.srs = osr.SpatialReference()
-        self.srs.SetFromUserInput(_srs)
-        logger.info(f"Global srs set to: {_srs}")
-
         # Declarations
+        self.srs = None
         self._exposure_data = None
         self._exposure_geoms = None
         self._exposure_grid = None
@@ -35,6 +32,7 @@ class BaseModel(metaclass=ABCMeta):
         self._vulnerability_data = None
         self._outhandler = None
 
+        self._set_model_srs()
         self._read_hazard_grid()
         self._read_vulnerability_data()
 
@@ -61,9 +59,10 @@ class BaseModel(metaclass=ABCMeta):
         )
         if not check_srs(self.srs, data.get_srs(), path.name):
             logger.warning(
-                f"Spatial reference of '{path.name}' does not match the global spatial reference"
+                f"Spatial reference of '{path.name}' ('{get_srs_repr(data.get_srs())}') \
+does not match the model spatial reference ('{get_srs_repr(self.srs)}')"
             )
-            logger.info(f"Reprojecting '{path.name}' to {get_srs_repr(self.srs)}")
+            logger.info(f"Reprojecting '{path.name}' to '{get_srs_repr(self.srs)}'")
             data = grid.reproject(data, self.srs.ExportToWkt())
         ## When all is done, add it
         self._hazard_grid = data
@@ -76,6 +75,39 @@ class BaseModel(metaclass=ABCMeta):
         logger.info("Executing vulnerability checks...")
 
         self._vulnerability_data = data
+
+    def _set_model_srs(self):
+        """_summary_"""
+
+        _srs = self._cfg.get("global.crs")
+        path = self._cfg.get("hazard.file")
+        if _srs is not None:
+            self.srs = osr.SpatialReference()
+            self.srs.SetFromUserInput(_srs)
+        else:
+            ## Inferring by 'sniffing'
+            kw = self._cfg.generate_kwargs("hazard.multiband")
+
+            gm = open_grid(
+                str(path),
+                **kw,
+            )
+
+            _srs = gm.get_srs()
+            if _srs is None:
+                if "hazard.crs" in self._cfg:
+                    _srs = osr.SpatialReference()
+                    _srs.SetFromUserInput(self._cfg.get("hazard.crs"))
+            self.srs = _srs
+
+        ## Simple check to see if it's not None
+        check_global_crs(
+            self.srs,
+            self._cfg.filepath.name,
+            path.name,
+        )
+
+        logger.info(f"Model srs set to: '{get_srs_repr(self.srs)}'")
 
     @abstractmethod
     def run():
