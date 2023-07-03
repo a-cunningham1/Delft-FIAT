15c2310aaf0be00a8ab6d350bcb83cdd86b44033
Brendan <brencodeert@outlook.com>
2023-07-03 13:58:03 +0200

    Working switch of output_dir

--- test/test_model.py
+++ test/test_model.py
@@ -1,9 +1,11 @@
 from delft_fiat.models import GeomModel
 
 import pytest
+from pathlib import Path
 
 
 def test_geom_model(tmpdir, cfg_event):
+    cfg_event.set_output_dir(str(tmpdir))
     model = GeomModel(cfg_event)
     model.run()
 
