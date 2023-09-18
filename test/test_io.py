e51b50300a0d8c4c3ab5751b524907e42c40bbd1
Brendan <brencodeert@outlook.com>
2023-09-18 14:22:45 +0200

    Added new tests; geom model and io

--- test/test_io.py
+++ test/test_io.py
@@ -1,5 +1,30 @@
+from delft_fiat.io import open_csv
+
 import pytest
 
 
-def test_csv():
-    pass
+def test_tabel(cfg_event):
+    tb = open_csv(cfg_event.get("vulnerability.file"))
+    assert len(tb.columns) == 3
+    assert len(tb.index) == 21
+    assert tb[9, "struct_2"] == 0.74
+    max_idx = max(tb.index)
+
+    # interpolate to refine the scale
+    tb.upscale(0.01, inplace=True)
+    assert len(tb.columns) == 3
+    assert len(tb) == 2001
+    assert tb[9, "struct_2"] == 0.74
+    assert tb[8.99, "struct_2"] == 0.7389
+    assert max(tb.index) == max_idx
+
+
+def test_geomsource(gm):
+    assert gm.count == 4
+    srs = gm.get_srs()
+    assert srs.GetAuthorityCode(None) == "4326"
+
+
+def test_gridsource(gr_event):
+    srs = gr_event.get_srs()
+    assert srs.GetAuthorityCode(None) == "4326"
