af3121b530b13410aa7ed9e1da77b8164812cd18
Brendan <brencodeert@outlook.com>
2023-10-06 16:28:17 +0200

    Some messaging

--- .build/linux64_build_env.sh
+++ .build/linux64_build_env.sh
@@ -1,30 +1,40 @@
+echo "Setup paths relative to this script"
 #!/usr/bin/bash 
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+# Setup conda stuff
+echo "Locate conda.."
+conda_executable=$(which conda)
+source $conda_executable
+
 # Extract the GDAL version number
+echo "Get GDAL version"
 version=$(gdalinfo --version | cut -d' ' -f2)
 # Remove the annoying comma
 version=${version%,}
 
 # Make the yaml and create the environment
-python $SCRIPTPATH/../make_env.py build
+echo "Create the fiat_build env"
+python $SCRIPTPATH/../make_env.py build -py 3.11.*
 mamba env create -f $SCRIPTPATH/../environment.yml
 
 # Set the appropriate env variables for GDAL
+echo "Setup some important GDAL env variables"
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
 
 # Install GDAL and FIAT
+echo "Install GDAL and FIAT"
 conda activate fiat_build
 pip install --no-cache-dir gdal==$version
-pip install -e .
+pip install -e $SCRIPTPATH/..
 conda deactivate
 
 # Clear the conda and pip cache
-conda_executable=$(which conda)
+echo "Clear the python cache"
 conda_base_dir=$(dirname $(dirname $conda_executable))
 rm -rf $conda_base_dir/pkgs/*
 pip cache purge
