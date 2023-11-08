b8672d60986b93d6b2aa5bcecd4b6388b37f194d
Brendan <brencodeert@outlook.com>
2023-11-08 13:26:46 +0100

    Fixed binary path

--- .build/runtime_hooks.py
+++ .build/runtime_hooks.py
@@ -4,7 +4,7 @@ import os
 import sys
 from pathlib import Path
 
-cwd = Path(sys.argv[0]).parent
+cwd = Path(sys.executable).parent
 
 os.environ["PROJ_LIB"] = str(Path(cwd, "bin", "share"))
 sys.path.append(str(Path(cwd, "bin", "share")))
