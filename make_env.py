bc6f87af61d030e2bd6f28d687ee46651e03b301
Brendan <brencodeert@outlook.com>
2024-01-12 13:33:09 +0100

    Update caching with build env; update make_env

--- make_env.py
+++ make_env.py
@@ -98,21 +98,22 @@ pip_deps = sorted(list(set(pip_deps)))
 
 # Make an exception for the build environment
 if args.profile == "build":
-    py = fnmatch.filter(conda_deps, "python*")
-    gd = fnmatch.filter(conda_deps, "gdal*")
-    conda_deps.remove(*gd)
-    if py:
-        conda_deps.remove(*py)
-    pip_deps += conda_deps
-    conda_deps = []
-    if py:
-        conda_deps += py
     if platform.system().lower() == "windows":
+        py = fnmatch.filter(conda_deps, "python*")
+        gd = fnmatch.filter(conda_deps, "gdal*")
+        conda_deps.remove(*gd)
+        if py:
+            conda_deps.remove(*py)
+        py = ["python==3.11.*"]
+        pip_deps += conda_deps
+        conda_deps = []
+        if py:
+            conda_deps += py
         pip_deps.append(
             "https://github.com/cgohlke/geospatial-wheels/releases/download/v2023.9.30/GDAL-3.7.2-cp311-cp311-win_amd64.whl",
         )
-    pip_deps = sorted(list(set(pip_deps)))
-    if platform.system().lower() == "windows":
+        pip_deps = sorted(list(set(pip_deps)))
+
         pip_deps.append("-e .")
 
 # add pip as a conda dependency if we have pip deps
