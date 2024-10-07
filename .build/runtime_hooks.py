ce3af47cf29bc3fcd24da5c092c2a49ff053c488
Brendan <brencodeert@outlook.com>
2024-10-07 10:57:03 +0200

    Update the runtime hook for newer PROJ versions

--- .build/runtime_hooks.py
+++ .build/runtime_hooks.py
@@ -9,5 +9,9 @@ cwd = Path(sys.executable).parent
 
 # Paths to libaries
 os.environ["GDAL_DRIVER_PATH"] = Path(cwd, "bin", "gdalplugins").as_posix()
+# Newer versions of GDAL and PROJ
+os.environ["PROJ_DATA"] = str(Path(cwd, "bin", "share"))
+# Older versions of GDAL and PROJ
 os.environ["PROJ_LIB"] = str(Path(cwd, "bin", "share"))
+# Append to path
 sys.path.append(str(Path(cwd, "bin", "share")))
